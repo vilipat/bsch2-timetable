8ac03eb233fcb4552aa026d9be483884acff6fed
Patrik Vilímek <none>
2023-11-06 14:06:31 +0100

    week period

---
+++ bsch2-timetable/Shared/WeekPeriod.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable.Shared
+{
+    public enum WeekPeriod
+    {
+        ODD,
+        EVEN,
+        ALL
+    }
+}
