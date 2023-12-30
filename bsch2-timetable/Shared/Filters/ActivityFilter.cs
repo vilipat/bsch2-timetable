e5606d6f8c17981c1212edfaada65b311309be3d
Patrik Vilímek <patrik.vilimek2@student.upce.cz>
2023-12-30 21:25:37 +0100

    filters

---
+++ bsch2-timetable/Shared/Filters/ActivityFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable.Shared.Filters
+{
+    public class ActivityFilter
+    {
+        public string Title { get; set; } = string.Empty;
+    }
+}
---
+++ bsch2-timetable/Shared/Filters/PersonFilter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Timetable.Shared.Filters
+{
+    public class PersonFilter
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
